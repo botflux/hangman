3a855b2ff8912ff6322ae1102e84dd22109ff11d
Victor <victor.mendele68@gmail.com>
2023-06-25 19:13:23 +0200

    create a vue front end

--- src/api/express/app.ts
+++ src/api/express/app.ts
@@ -15,15 +15,7 @@ export function createApp () {
   }))
 
   app.get ("/", (_req, res) => {
-    res.render ("index.ejs", {
-      game: gameToGameView(game)
-    })
-  })
-
-  app.post ("/try-letter", (req, res) => {
-    game = tryLetter(game, req.body.letter)
-
-    res.render ("main.ejs", { game: gameToGameView(game) })
+    res.render ("index.ejs")
   })
 
   app.get ("/game", (_req, res) => {
@@ -39,45 +31,18 @@ export function createApp () {
   return app
 }
 
-type GameView = {
-  state: GameState
-  letters: string[]
-  maxFailedAttempts: string
-  failedAttempts: string
-}
-
-function gameToGameView (game: Game): GameView {
-  const {
-    state,
-    word,
-    maxFailedAttempts,
-    failedAttempts
-  } = game
-
-  return {
-    state,
-    letters: word.letters.map (
-      value => isLetterDiscovered(value)
-        ? value.letter
-        : ""
-    ),
-    maxFailedAttempts: maxFailedAttempts.toString(),
-    failedAttempts: failedAttempts.toString()
-  }
-}
-
 type LetterStateView = 
   | { type: "hidden" }
   | { type: "discovered", letter: string }
 
-type ApiGameView = {
+type GameView = {
   state: GameState
   letters: LetterStateView[]
   maxFailedAttempts: number
   failedAttempts: number
 }
 
-function gameView (game: Game): ApiGameView {
+function gameView (game: Game): GameView {
   const {
     state,
     word,
